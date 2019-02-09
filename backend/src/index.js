74fadd99f25e48f79928fe654658af538611bc60
Karen Gomes <kngs@ic.ufal.br>
2019-02-09 20:31:27 -0300

    feat(backend): using socket.io real-time framework

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const { USER, PASSWORD } = require("../constants");
 
 const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 mongoose.connect(
   `mongodb://${USER}:${PASSWORD}@ds129045.mlab.com:29045/twitter-clone-backend`,
@@ -11,9 +15,16 @@ mongoose.connect(
   }
 );
 
-app.use(express.json());
-app.use(require('./routes'));
+app.use((req, res, next) => {
+  req.io = io;
 
-app.listen(3000, () => {
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes"));
+
+server.listen(3000, () => {
   console.log("Server started on port 3000");
 });
