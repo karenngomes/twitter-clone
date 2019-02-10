3041cab75166674a3b387c2903a3fdfff035e133
Karen Gomes <kngs@ic.ufal.br>
2019-02-10 01:11:56 -0300

    feat(mobile): create project structure and login route

---
+++ mobile/src/routes.js
@@ -0,0 +1,7 @@
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+
+import Login from "./pages/Login";
+
+const Routes = createAppContainer(createSwitchNavigator({ Login }));
+
+export default Routes;
