055e34b77389028ed735a58384da34cbd30139c4
Karen Gomes <kngs@ic.ufal.br>
2019-02-10 03:32:24 -0300

    feat(mobile): sending a new tweet to backend

--- mobile/src/pages/New.js
+++ mobile/src/pages/New.js
@@ -1,16 +1,65 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  TextInput,
+  AsyncStorage
+} from "react-native";
+import api from "../services/api";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 export default class New extends Component {
   static navigationOptions = {
     header: null
   };
 
+  state = {
+    newTweet: ""
+  };
+
+  goBack = () => {
+    this.props.navigation.pop();
+  };
+
+  handleTweet = async () => {
+    const content = this.state.newTweet;
+    const author = await AsyncStorage.getItem("@TwitterMobile:username");
+
+    await api.post("tweets", { content, author });
+
+    this.goBack();
+  };
+
+  handleInputChange = newTweet => {
+    this.setState({ newTweet });
+  };
+
   render() {
+    const { newTweet } = this.state;
     return (
-      <View style={styles.container}>
-        <Text> textInComponent </Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={this.goBack}>
+            <Icon name="close" size={24} color="#4BB0EE" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={this.handleTweet}>
+            <Text style={styles.buttonText}>Tweetar</Text>
+          </TouchableOpacity>
+        </View>
+        <TextInput
+          style={styles.input}
+          multiline
+          placeholder="O que está acontecendo?"
+          placeholderTextColor="#999"
+          value={newTweet}
+          onChangeText={this.handleInputChange}
+          returnKeyType="send"
+          onSubmitEditing={this.handleTweet}
+        />
+      </SafeAreaView>
     );
   }
 }
