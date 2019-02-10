f1e91b620d845c6b9be4cc2a715351da3673f497
Karen Gomes <kngs@ic.ufal.br>
2019-02-09 21:06:05 -0300

    feat(frontend): using react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Timeline from "./pages/Timeline";
 
 class App extends Component {
   render() {
-    return <h2>Hello World</h2>;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/timeline" component={Timeline} />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
