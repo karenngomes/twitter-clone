e4ee472ca93ab6f5e8c114e94e53ea329ac7d3d3
Karen Gomes <kngs@ic.ufal.br>
2019-02-09 22:31:00 -0300

    feat(frontend): created login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
 
+import twitterLogo from "../twitter.svg";
+import "./Login.css";
+
 export default class Login extends Component {
   render() {
-    return <h1>Login</h1>;
+    return (
+      <div className="login-wrapper">
+        <img src={twitterLogo} alt="Twitter Logo" />
+        <form>
+          <input placeholder="Nome de usuário" />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
