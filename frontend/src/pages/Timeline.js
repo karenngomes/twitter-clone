c98b8b13e995a94a4483c847058a8394cafae1e7
Karen Gomes <kngs@ic.ufal.br>
2019-02-10 00:04:38 -0300

    feat(frontend): add socket.io

--- frontend/src/pages/Timeline.js
+++ frontend/src/pages/Timeline.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import api from "../services/api";
+import socket from "socket.io-client";
 
 import twitterLogo from "../twitter.svg";
 import "./Timeline.css";
@@ -13,10 +14,27 @@ export default class Timeline extends Component {
   };
 
   async componentDidMount() {
+    this.subscribeToEvents();
+
     const response = await api.get("tweets");
 
     this.setState({ tweets: response.data });
   }
+
+  subscribeToEvents = () => {
+    const io = socket("http://localhost:3000");
+
+    io.on("tweet", data => {
+      this.setState({ tweets: [data, ...this.state.tweets] });
+    });
+    io.on("like", data => {
+      this.setState({
+        tweets: this.state.tweets.map(tweet =>
+          tweet._id === data._id ? data : tweet
+        )
+      });
+    });
+  };
 
   handleNewTweet = async e => {
     if (e.keyCode !== 13) return;
