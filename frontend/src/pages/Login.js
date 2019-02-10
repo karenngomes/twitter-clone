053e1e71db532a6306657de3b0d5f0d3186bca73
Karen Gomes <kngs@ic.ufal.br>
2019-02-09 23:04:03 -0300

    feat(frontend): get username and redirect to timeline page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,12 +4,37 @@ import twitterLogo from "../twitter.svg";
 import "./Login.css";
 
 export default class Login extends Component {
+  state = {
+    username: ""
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { username } = this.state;
+
+    if (!username.length) return;
+
+    localStorage.setItem("@GoTwitter:username", username);
+
+    this.props.history.push("./timeline");
+  };
+
+  handleInputChange = e => {
+    this.setState({ username: e.target.value });
+  };
+
   render() {
+    const { username } = this.state;
     return (
       <div className="login-wrapper">
         <img src={twitterLogo} alt="Twitter Logo" />
-        <form>
-          <input placeholder="Nome de usuário" />
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={username}
+            onChange={this.handleInputChange}
+            placeholder="Nome de usuário"
+          />
           <button type="submit">Entrar</button>
         </form>
       </div>
